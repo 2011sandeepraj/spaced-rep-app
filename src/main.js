f496173a6289091d2273c5e4d2f9366ac00c109f
sandeepraj <sandeepraj.tandon@gov.bc.ca>
2021-01-15 17:21:21 -0800

    basic form with a button only

--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/md-light-indigo/theme.css' // theme
+import 'primevue/resources/primevue.min.css' // core css
+import 'primeicons/primeicons.css' // icons
+import 'primeflex/primeflex.css'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ColumnGroup from 'primevue/columngroup'
+import ToggleButton from 'primevue/togglebutton'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router)
+
+app.use(PrimeVue)
+app.use(ToastService)
+
+app.component('InputText', InputText)
+app.component('Button', Button)
+app.component('Toast', Toast)
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('ColumnGroup', ColumnGroup)
+app.component('ToggleButton', ToggleButton)
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home p-grid">
+
+    <form class="p-col-6 p-offset-3 p-fluid">
+      <div class="p-field">
+        <label  for="question">Question</label>
+       <InputText id="question" class="p-field"/>
+      </div>
+      <div class="p-field">
+        <label  for="answer">Answer</label>
+       <InputText id="answer" class="p-field"/>
+      </div>
+      <Button> Add New Card </Button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
+  },
+  methods: {
+    createCard () {
+
+    }
   }
 }
 </script>
